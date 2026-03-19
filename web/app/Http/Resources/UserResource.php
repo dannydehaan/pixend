1dc0d7fc4d10bb69ddd0feeeb460b695485a718a
Danny the Rooster <danny@pixels.nl>
2026-03-19 02:51:14 +0100

    WIP

--- web/app/Http/Resources/UserResource.php
+++ web/app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'is_premium' => $this->is_premium,
         ];
     }
 }
